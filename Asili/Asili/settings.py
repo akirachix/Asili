9709973c69abfa1e4e966b084cca3278d9554682
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-10-22 21:00:24 +0300

    deployment changes

--- Asili/Asili/settings.py
+++ Asili/Asili/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,5 +147,5 @@ MEDIA_URL =  '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # django_heroku.settings(locals())
-
+import django_heroku
 django_heroku.settings(locals())
