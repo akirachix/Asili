36f2ceb8375b38f42d1f7ac72740c716e75a416f
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-10-30 17:10:59 +0300

    profile page

--- Asili/api/models.py
+++ Asili/api/models.py
@@ -45,49 +45,44 @@ class Cloth(models.Model):
     gender = models.CharField(max_length= 10,choices=GENDER_CHOICES, null = True ,default='SOME STRING')
     def __str__(self):
          return self.gender
-# Create your models here.
-
-    first_name = models.CharField(max_length=10, null = True)
-    last_name = models.CharField(max_length=10, null = True)
-    password = models.CharField(max_length=10) 
-    email = models.EmailField()
+# Create your models here
   
 
-class Designer(models.Model):
+# class Designer(models.Model):
 
-    first_name = models.CharField(max_length=10, null = True)
-    last_name = models.CharField(max_length=10, null = True)
-    password = models.CharField(max_length=10) 
-    # profile = models.ImageField()
-    email = models.EmailField()
-    # gender = models.CharField(max_length=10)
+#     first_name = models.CharField(max_length=10, null = True)
+#     last_name = models.CharField(max_length=10, null = True)
+#     password = models.CharField(max_length=10) 
+#     # profile = models.ImageField()
+#     email = models.EmailField()
+#     # gender = models.CharField(max_length=10)
 
 
-class Categories(models.Model):
-    new = models.ImageField(upload_to="pictures/")
-    men = models.ImageField(upload_to="pictures/")
-    women = models.ImageField(upload_to="pictures/")
-    kids = models.ImageField(upload_to="pictures/")
-    mostpopular = models.ImageField(upload_to="pictures/")
+# class Categories(models.Model):
+#     new = models.ImageField(upload_to="pictures/")
+#     men = models.ImageField(upload_to="pictures/")
+#     women = models.ImageField(upload_to="pictures/")
+#     kids = models.ImageField(upload_to="pictures/")
+#     mostpopular = models.ImageField(upload_to="pictures/")
     
-class Men(models.Model):
-    trousers = models.ImageField()
-    shirts = models.ImageField()
-    jackets = models.ImageField()
-    suits = models.ImageField()
+# class Men(models.Model):
+#     trousers = models.ImageField()
+#     shirts = models.ImageField()
+#     jackets = models.ImageField()
+#     suits = models.ImageField()
 
-class Women(models.Model):
-    trousers = models.ImageField()
-    shirts = models.ImageField()
-    jackets = models.ImageField()
-    dress = models.ImageField()
-    skirts = models.ImageField()
+# class Women(models.Model):
+#     trousers = models.ImageField()
+#     shirts = models.ImageField()
+#     jackets = models.ImageField()
+#     dress = models.ImageField()
+#     skirts = models.ImageField()
 
-class Kids(models.Model):
-    trousers = models.ImageField()
-    shirts = models.ImageField()
-    jackets = models.ImageField()
-    dress = models.ImageField()
-    suits = models.ImageField()
+# class Kids(models.Model):
+#     trousers = models.ImageField()
+#     shirts = models.ImageField()
+#     jackets = models.ImageField()
+#     dress = models.ImageField()
+#     suits = models.ImageField()
 
 
