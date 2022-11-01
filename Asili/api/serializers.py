4031f107a137d41c40462189b110e0db60078bf5
farsosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-11-01 09:56:52 +0300

    changes

--- Asili/api/serializers.py
+++ Asili/api/serializers.py
@@ -5,35 +5,35 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
    class Meta:
        model = User
-       fields = ("id","first_name", "last_name", "email", "password")
+       fields = ("first_name", "last_name", "email", "password")
 
 
 class DesignerSerializer(serializers.ModelSerializer):
    class Meta:
        model = Designer
-       fields = ("id","first_name", "last_name", "email", "password")
+       fields = ("first_name", "last_name", "email", "password")
  
  
 class CategoriesSerializer(serializers.ModelSerializer):
    class Meta:
        model = Categories
-       fields = ("id","image", "type", "wearer")
+       fields = ("image", "type", "wearer")
  
       
  
 class MenSerializer(serializers.ModelSerializer):
    class Meta:
        model = Men
-       fields = ("id","image", "type")
+       fields = ("image", "type")
  
 class WomenSerializer(serializers.ModelSerializer):
    class Meta:
        model = Women
-       fields = ("id","image", "type")
+       fields = ("image", "type")
   
 class KidsSerializer(serializers.ModelSerializer):
    class Meta:
        model = Kids
-       fields = ("id","image", "type")
+       fields = ("image", "type")
  
  
