f6a3edecd9901400c73c7428b2942ec2dbb4cf38
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-11-03 11:54:21 +0300

    changes

--- Asili/Asili/settings.py
+++ Asili/Asili/settings.py
@@ -10,6 +10,15 @@ import os
 from pathlib import Path
 import dj_database_url
 import django_heroku
+# import cloudinary
+# import cloudinary.uploader
+# import cloudinary.api
+
+# cloudinary.config( 
+#   cloud_name = "dg8d1digi", 
+#   api_key = "844831737131679", 
+#   api_secret = "GYr8ob12OPaGByDiRMG38O8VhOM" 
+# )
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,6 +42,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'whitenoise.runserver_nostatic',
+    'cloudinary'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
